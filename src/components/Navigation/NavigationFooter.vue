d693fcfd1f2b0e4a036c7f70847a68ff2cad3b0c
romarioscbl <romario.sarmiento@buildinglink.com>
2024-10-21 23:32:05 -0500

    Add social media icons in home

--- src/components/Navigation/NavigationFooter.vue
+++ src/components/Navigation/NavigationFooter.vue
@@ -1,5 +1,11 @@
 <script setup></script>
 
 <template>
-  <footer>Aaaa</footer>
+  <footer class="text-center py-4">
+    <div>
+      <p class="text-sm font-light text-gray-400">
+        Developed by Romario Sarmiento in 2024
+      </p>
+    </div>
+  </footer>
 </template>
--- src/components/SectionHome.vue
+++ src/components/SectionHome.vue
@@ -1,13 +1,41 @@
+<script setup>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons'
+</script>
+
 <template>
   <section class="container mx-auto py-16">
     <div class="grid grid-cols-2 items-center">
-      <div class="space-y-2">
-        <h3 class="text-3xl font-semibold text-slate-100">I am Romario</h3>
-        <h1 class="text-6xl font-bold text-violet-400">Full Stack Developer</h1>
-        <p class="text-xl font-normal text-slate-100">
+      <div>
+        <h3 class="text-3xl font-semibold text-slate-100 mb-4">I am Romario</h3>
+        <h1 class="text-6xl font-bold text-violet-400 mb-4">
+          Full Stack Developer
+        </h1>
+        <p class="text-xl font-normal text-slate-100 mb-8">
           I suggest ideas and solve problems by developing efficient,
           user-friendly and maintainable applications over time
         </p>
+        <div class="flex flex-row items-center gap-4">
+          <button
+            class="transition-colors px-6 py-3 rounded-full border text-violet-500 border-violet-500 hover:text-white hover:bg-violet-500"
+          >
+            Download CV
+          </button>
+          <a
+            class="transition-colors flex items-center justify-center rounded-full border text-violet-500 border-violet-500 hover:text-white hover:bg-violet-500 w-8 h-8"
+            href="https://www.linkedin.com/in/romariosarmiento/"
+            target="_blank"
+          >
+            <FontAwesomeIcon :icon="faLinkedinIn" />
+          </a>
+          <a
+            class="transition-colors flex items-center justify-center rounded-full border text-violet-500 border-violet-500 hover:text-white hover:bg-violet-500 w-8 h-8"
+            href="https://github.com/romariosc15"
+            target="_blank"
+          >
+            <FontAwesomeIcon :icon="faGithub" />
+          </a>
+        </div>
       </div>
       <div class="mx-auto">
         <img
